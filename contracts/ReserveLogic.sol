88864776e542e7d42249a02b08c9e4b822ca36a8
Blockchain-Mexico <olmecadex@gmail.com>
2023-06-05 14:23:14 -0600

    Hardhat set up and truffle figuerion out

--- contracts/ReserveLogic.sol
+++ contracts/ReserveLogic.sol
@@ -2,14 +2,12 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/math/PercentageMath.sol";
-import "@openzeppelin/contracts/utils/math/WadRayMath.sol";
+//import "@openzeppelin/contracts/utils/math/WadRayMath.sol";
 
 library ReserveLogic {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-    using PercentageMath for uint256;
-    using WadRayMath for uint256;
+    // using WadRayMath for uint256;
 
     // Pool
     event Deposit(
